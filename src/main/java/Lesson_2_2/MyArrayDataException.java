874c45fd355b199939e7f27f241da541c468cbc6
Roman-K-S <70799864+Roman-K-S@users.noreply.github.com>
2021-10-16 20:03:33 +0300

    Lesson 8

--- src/main/java/Lesson_2_2/MyArrayDataException.java
+++ src/main/java/Lesson_2_2/MyArrayDataException.java
@@ -3,5 +3,6 @@ package Lesson_2_2;
 public class MyArrayDataException extends Throwable {
     public MyArrayDataException(String s) {
         super(s);
+
     }
 }
--- src/main/java/Lesson_2_2/MyArraySizeException.java
+++ src/main/java/Lesson_2_2/MyArraySizeException.java
@@ -3,5 +3,6 @@ package Lesson_2_2;
 public class MyArraySizeException extends Exception {
     public MyArraySizeException(String s) {
         super(s);
+
     }
 }
