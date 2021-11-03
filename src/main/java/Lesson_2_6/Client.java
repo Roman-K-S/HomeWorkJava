029e7478cccbe06f1e5612d0c46702ae822e69f8
Roman-K-S <70799864+Roman-K-S@users.noreply.github.com>
2021-11-03 21:10:22 +0300

    Lesson_2_6

---
+++ src/main/java/Lesson_2_6/Client.java
@@ -0,0 +1,90 @@
+package Lesson_2_6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private final String SERVER_ADDR = "localhost";
+    private final int SERVER_PORT = 8189;
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public Client(){
+        try {
+            openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openConnection() throws IOException {
+        socket = new Socket(SERVER_ADDR, SERVER_PORT);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        String strFromServer = in.readUTF();
+                        if (strFromServer.equalsIgnoreCase("/end")){
+                            closeConnection();
+                            break;
+                        }
+                        System.out.println(strFromServer);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void closeConnection() {
+        try {
+            in.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            out.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        try {
+            socket.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String msgInput) {
+        if (!msgInput.isEmpty()){
+            try {
+                out.writeUTF(msgInput);
+            } catch (IOException e){
+                e.printStackTrace();
+                System.out.println("Ошибка при передаче сообщения.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner msgInput = new Scanner(System.in);
+
+        Client cl = new Client();
+
+        while (msgInput.toString() != "\\end") {
+            if (!msgInput.toString().isEmpty()) {
+                cl.sendMessage(msgInput.nextLine());
+            }
+        }
+    }
+}
---
+++ src/main/java/Lesson_2_6/Server.java
@@ -0,0 +1,29 @@
+package Lesson_2_6;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        Socket socket = null;
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            System.out.println("Сервер запущен, ожидаю клиента...");
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключился.");
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            while (true){
+                String str = in.readUTF();
+                if (str.equals("/end")){
+                    break;
+                }
+                out.writeUTF("Лови эхо: " + str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
