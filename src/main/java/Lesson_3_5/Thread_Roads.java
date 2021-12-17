980e1c015979df00efddf727d3c42308addcc92b
Roman-K-S <70799864+Roman-K-S@users.noreply.github.com>
2021-12-17 23:09:06 +0300

    Lesson_3_5 Start Together cars Print Race Start Semaphore Tunnel class half cars Print Race Finish Add Winner

--- src/main/java/Lesson_3_5/Thread_Roads.java
+++ src/main/java/Lesson_3_5/Thread_Roads.java
@@ -1,8 +1,17 @@
 package Lesson_3_5;
 
-public class Thread_part_two {
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Thread_Roads {
     public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+    public static CyclicBarrier ready_Steady_Go = new CyclicBarrier(Thread_Roads.CARS_COUNT);
+    public static CountDownLatch race_Start = new CountDownLatch(Thread_Roads.CARS_COUNT);
+    public static CountDownLatch race_Finish = new CountDownLatch(Thread_Roads.CARS_COUNT);
+
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
@@ -12,7 +21,9 @@ public class Thread_part_two {
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+        race_Start.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        race_Finish.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
