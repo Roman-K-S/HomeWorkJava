1366e9b10ebb3a63ea649178e07bdf4bef5ee34f
Roman-K-S <70799864+Roman-K-S@users.noreply.github.com>
2021-12-27 22:16:10 +0300

    Lesson_3_7

--- src/main/java/Lesson_3_7/HappyNewYear.java
+++ src/main/java/Lesson_3_7/HappyNewYear.java
@@ -1,40 +1,16 @@
 package Lesson_3_7;
 
 public class HappyNewYear {
-    private int year;
-    private String congratulation;
 
-    public HappyNewYear(int year, String congratulation) {
-        this.year = year;
-        this.congratulation = congratulation;
-    }
-
-    @test(priority = 1)
-    public int getYear() {
-        return year;
+    public HappyNewYear(){
     }
 
     @BeforeSuite
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    @AfterSuite
-    public String getCongratulation() {
-        return congratulation;
-    }
-
-    @BeforeSuite
-    public void setCongratulation(String congratulation) {
-        this.congratulation = congratulation;
+    public void printGreetings(String greeting) {
+        System.out.println(greeting);
     }
 
     @test(priority = 2)
-    public void printGreetings() {
-        System.out.println("С наступающим новым годом!");
-    }
-
-    @test(priority = 3)
     public void printChristmasTree() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10 - i; j++)
@@ -43,5 +19,11 @@ public class HappyNewYear {
                 System.out.print("*");
             System.out.println();
         }
+        System.out.println();
+    }
+
+    @AfterSuite
+    public void printCongratulation (String congratulation){
+        System.out.println(congratulation);
     }
 }
